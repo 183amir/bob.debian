f4bc64cb15c131a6bc5f3af5744b525a5eeac1e6
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-04-10 13:16:30 +0200

    VLFeat ppa: add patch for library soname version

--- vlfeat/local.sh
+++ vlfeat/local.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
-# Laurent El Shafey <laurent.el-shafey@idiap.ch>
-# Fri Mar 30 20:31:10 2012 +0200
+# Andre Anjos <andre.anjos@idiap.ch>
+# Fri 30 Mar 2012 15:42:02 CEST
 
-# Creates a new debian package for VLFeat based on Laurent
+# Creates a new debian package for VLFeat based on Laurent's instructions
+# and findings.
 
 # Configure here your parameters for the package you are building
 vlfeat_version="0.9.14"
@@ -15,25 +16,21 @@ tar -xvzf vlfeat-${vlfeat_version}.tar.gz
 cp vlfeat-${vlfeat_version}.tar.gz vlfeat_${vlfeat_version}.orig.tar.gz
 mv vlfeat-${vlfeat_version} vlfeat.clone
 
-
 date=`date +"%a, %d %b %Y %H:%M:%S %z"`
 echo "Today                   : ${date}"
 echo "VLFeat version          : ${vlfeat_version}"
 
-for distro in precise oneiric natty maverick lucid; do
+for distro in unknown; do
+  rm -rf vlfeat.local *.build
   ppa_version="${vlfeat_version}-0~ppa${ppa_iteration}~${distro}1"
   echo "Biometrics PPA version  : ${ppa_version}"
 
   echo "Generating source packages for Ubuntu '${distro}'..."
-  cp -r vlfeat.clone vlfeat_${vlfeat_version}
-  cd vlfeat_${vlfeat_version}
+  cp -r vlfeat.clone vlfeat.local
+  cd vlfeat.local
   cp -r ../debian .
   sed -i -e "s/@VERSION@/${vlfeat_version}/g" debian/rules
   sed -i -e "s/@VERSION@/${vlfeat_version}/g;s/@PPA_VERSION@/${ppa_version}/g;s/@DATE@/${date}/g;s/@DISTRIBUTION@/${distro}/g" debian/changelog
-  debuild -k${gpg_key} -sa -S;
+  debuild -us -uc -b
   cd ..
-  rm -rf vlfeat_${vlfeat_version} 
 done
-
-rm -rf vlfeat.clone
-rm vlfeat-${vlfeat_version}.tar.gz
