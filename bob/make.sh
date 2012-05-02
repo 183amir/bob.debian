4ac16d050e13b5f82c542e34337b1ae3a59851fc
Andre Anjos <andre.anjos@idiap.ch>
2012-05-02 14:45:42 +0200

    Bob upgrade

--- bob/make.sh
+++ bob/make.sh
@@ -6,7 +6,7 @@
 
 # Configure here your parameters for the package you are building
 soversion="1.0"
-version="${soversion}.2"
+version="${soversion}.3"
 package="bob_${version}"
 ppa_iteration="1"
 gpg_key="A2170D5D"
