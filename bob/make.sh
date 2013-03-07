ba50b6e28e5f3e81c19cef2fc7d1923d3a23408a
Andre Anjos <andre.dos.anjos@gmail.com>
2013-03-07 19:01:42 +0100

    Reduce number of supported platforms; Bob-1.1.3

--- bob/make.sh
+++ bob/make.sh
@@ -6,7 +6,7 @@
 
 # Configure here your parameters for the package you are building
 soversion="1.1"
-version="${soversion}.2"
+version="${soversion}.3"
 package="bob_${version}"
 ppa_iteration="1"
 #gpg_key="A2170D5D"; # Andre
@@ -14,7 +14,7 @@ gpg_key="E0CE7EF8"; # Laurentes
 #source_shipped=0; #if you set this to 1, all changes will ship with srcs
 source_shipped=1; #if you set this to 0, all changes will ship w/o srcs
 #distros="quantal";
-distros="quantal precise oneiric natty maverick lucid";
+distros="quantal precise oneiric lucid";
 
 if [ ! -e ${package}.orig.tar.gz ]; then
   #wget https://launchpad.net/~biometrics/+archive/bob/+files/bob_1.1.1.orig.tar.gz
