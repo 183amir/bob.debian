e7061c4c1e06b932e460777652305be6ab13953f
Andre Anjos <andre.dos.anjos@gmail.com>
2014-04-22 10:37:00 +0200

    Add trusty specific files

--- bob/make.sh
+++ bob/make.sh
@@ -8,13 +8,13 @@
 soversion="1.2"
 version="${soversion}.2"
 package="bob_${version}"
-ppa_iteration="1"
+ppa_iteration="3"
 gpg_key="A2170D5D"; # Andre
 #gpg_key="E0CE7EF8"; # Laurentes
-source_shipped=1; #if you set this to 1, all changes will ship with srcs
-#source_shipped=0; #if you set this to 0, all changes will ship w/o srcs
-#distros="saucy";
-distros="saucy raring quantal precise lucid";
+#source_shipped=1; #if you set this to 1, all changes will ship with srcs
+source_shipped=0; #if you set this to 0, all changes will ship w/o srcs
+distros="trusty";
+#distros="trusty saucy raring quantal precise lucid";
 
 if [ ! -e ${package}.orig.tar.gz ]; then
   wget http://www.idiap.ch/software/bob/packages/bob-${version}.tar.gz;
