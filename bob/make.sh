a7d0728d1dcfe29bf72aceba6c19606c7e6f6e44
Pavel Korshunov <pavel.korshunov@idiap.ch>
2015-08-21 23:25:53 +0200

    Working version for Ubuntu/trusty, tested on launchpad

--- bob/make.sh
+++ bob/make.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 # Andre Anjos <andre.anjos@idiap.ch>
 # Sun Apr  1 23:05:41 CEST 2012
+# Pavel Korshunov <pavel.korshunov@idiap.ch>
+# Sun Aug 18 22:42:40 CEST 2015
 
 # Creates a new debian package for Bob
 
 # Configure here your parameters for the package you are building
-soversion="1.2"
-version="${soversion}.2"
-package="bob_${version}"
-ppa_iteration="6"
-gpg_key="A2170D5D"; # Andre
+soversion="2.0"
+version="${soversion}.4"
+package="bob_${version}-1"
+#change ppa_iteration for every new update on ppa launchpad
+ppa_iteration="5"
+#change your GPG/PGP key here
+gpg_key="5EEC234C"; Pavel
+#gpg_key="A2170D5D"; # Andre
 #gpg_key="E0CE7EF8"; # Laurentes
 #source_shipped=1; #if you set this to 1, all changes will ship with srcs
 source_shipped=0; #if you set this to 0, all changes will ship w/o srcs
-distros="lucid";
+distros="trusty";
 #distros="trusty saucy raring quantal precise lucid";
 
 if [ ! -e ${package}.orig.tar.gz ]; then
-  wget http://www.idiap.ch/software/bob/packages/bob-${version}.tar.gz;
-  tar xfz bob-${version}.tar.gz;
-  rm -f bob-${version}.tar.gz;
-  mv bob-${version} ${package}.orig;
+  echo "${package}.orig.tar.gz does not exist, so we are creating it"
+  if [ ! -e bob-${version}.zip ]; then
+    #download bob from the Pypi morror on debian (not MD5 checksums here)
+    echo "bob-${version}.zip does not exist, so downloading it from debian's mirro on pypi.."
+    wget http://pypi.debian.net/bob/bob-${version}.zip
+  fi
+  #we need to create a folder named in a certain way, so debian packaging
+  #environment can understand it
+  unzip bob-${version}.zip
+  mv -f bob-${version} ${package}.orig
+  rm -f bob-${version}.zip
+
+  #download all dependencies and put inside our future tarball
+  cd ${package}.orig
+  echo "running pip install with --download option to get all dependancies
+  locally"
+  while read req; do
+    echo "Installing \`${req}'..."
+    #pip install --download="." -v -v -v --no-use-wheel --no-deps --no-compile --find-links="." --install-option sdist "${req}"
+    IFS=' ' read -a parsed_req <<< "${req}"
+    wget http://pypi.debian.net/${parsed_req[0]}/${parsed_req[0]}-${parsed_req[2]}.zip
+    status=$?
+    echo "pip returned status = ${status}"
+    if [  "${status}" != 0  ]; then
+      echo "Installation of package ${req} failed; aborting"
+      exit ${status};
+    fi
+  done <requirements.txt
+  cd ..
+
+  #tarball the folder with all the sources
   tar cfz ${package}.orig.tar.gz ${package}.orig;
 fi
 
+#make sure date generates days of the week in English (the Locale on your
+#Machine)
 date=`date +"%a, %d %b %Y %H:%M:%S %z"`
 echo "Today                   : ${date}"
 echo "Bob version             : ${version}"
 
+#copies all debian files related to the distribution from os.files folder
+#into debian folder
 for distro in ${distros}; do
-  ppa_version="${version}-0~ppa${ppa_iteration}~${distro}1"
+  ppa_version="${version}-1-0~ppa${ppa_iteration}~${distro}1"
   echo "Biometrics PPA version  : ${ppa_version}"
 
   echo "Generating source packages for Ubuntu '${distro}'..."
@@ -46,13 +82,14 @@ for distro in ${distros}; do
       cp ${CPOPT} ../os.files/${specific}.${distro} debian/${specific}
     fi
   done
-  sed -i -e "s/@VERSION@/${version}/g;s/@SOVERSION@/${soversion}/g" debian/rules
+  #update all versions and date in the changelog file, so that correct debian
+  #packages are created. Make sure your Day of the week in Locale is in English
   sed -i -e "s/@VERSION@/${version}/g;s/@PPA_VERSION@/${ppa_version}/g;s/@DATE@/${date}/g;s/@DISTRIBUTION@/${distro}/g" debian/changelog
   if [ "${source_shipped}" = "1" ]; then
-    debuild -k${gpg_key} -sa -S;
+    debuild -i -k${gpg_key} -sa -S;
     source_shipped=0;
   else
-    debuild -k${gpg_key} -sd -S;
+    debuild -i -k${gpg_key} -sd -S;
   fi
   cd ..
   rm -rf ${package}
